821b833e304977982618f70b29572c93c4a88651
Miggy <hedcenforever@gmail.com>
2018-03-23 09:47:56 +0800

    Changes made: -Updated Spider to allow to craw recursively until the MAX_PAGE_NUMBER is reached -added 3 .txt files: 	-desiredSpiderWordOutput.txt showcases the words than are taken from all the necessary pages, needs to be sent to DB 	-desiredSpiderURLOutput.txt showcases the URL in each page that has been crawled, tbd on what to do with it 	-listOfURLsFromMainPage.txt showcases the URL from the first page to check if the 30 pages have been searched appropriately Next Up: -Need to get meta data to find when the page has been last updated and maybe some other info per word -Delete unecessary characters i.e. ; , " : . etc

--- Spider.java
+++ Spider.java
@@ -5,8 +5,6 @@
 //to compile:	javac -cp lib/htmlparser.jar Spider.java
 //to run:		java -cp lib/htmlparser.jar:. Spider
 
-import java.util.Vector;
-
 import org.htmlparser.beans.StringBean;
 import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
@@ -14,43 +12,57 @@ import org.htmlparser.filters.AndFilter;
 import org.htmlparser.filters.NodeClassFilter;
 import org.htmlparser.tags.LinkTag;
 import org.htmlparser.util.NodeList;
-import java.util.StringTokenizer;
 import org.htmlparser.beans.LinkBean;
-
-import java.net.URL;
 import org.htmlparser.Parser;
 import org.htmlparser.util.ParserException;
+
+import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+
+import java.net.URL;
 
 
 public class Spider {
 	
 	private String url;
-	private Vector<String> linkResults;
-	private Vector<String> wordResults;
 
-	Spider(String _url) {
-		url = _url;
-		linkResults = new Vector<String>();
-		wordResults = new Vector<String>();
+	//results from crawl
+	private List<String> linkResults = new LinkedList<String>();
+	private Vector<String> wordResults = new Vector<String>();
+
+	//information to be used by the recursive function
+    private Set<String> pagesVisited = new HashSet<String>();
+    private List<String> pagesToVisit = new LinkedList<String>();
+	private static final int MAX_PAGES_TO_SEARCH = 30;
+
+	//Constructor
+	Spider() {
+
 	}
 
 	//testing with manual input
 	public static void main(String [] args){
-		Spider testCrawl = new Spider("http://www.cse.ust.hk/");
+		Spider testCrawl = new Spider();
 		
-		testCrawl.crawlURL("http://www.cse.ust.hk/",1);
-		
-		try{
-			testCrawl.printWords();
-			//testCrawl.printLinks();
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
+		testCrawl.search("http://www.cse.ust.hk/");
 		return;
 	}
 
-	public void crawlURL(String url, int numPages) {
+
+	/**
+	* This performs all the work. It makes an HTTP request, checks the response, and then gathers
+	* up all the links on the page. Perform a searchForWord after the successful crawl
+	* 
+	* @param url
+	*            - The URL to visit
+	* @return whether or not the crawl was successful
+	*/
+	//Sifts through URL pages and sends it to the database
+	public void crawl(String url) {
 		//given the url, it will crawl it and recursively crawl on the pages that it links to
 		//
 		//NOTES FROM PROJECT REQUIREMENTS:
@@ -62,28 +74,81 @@ public class Spider {
 		//	b. if url already exist but last modification date of the url is later than
 		//	that recorded in the index, go ahead and retrieve the url; otherwise ignore
 		//	c. handle cyclic links gracefully 
-		try{
-			extractKeywords(url);
-			extractLinks(url);
+		
+
+		try {
+			this.extractLinks(url);
+			this.extractKeywords(url);
 		}
-		catch (ParserException e){
-			e.printStackTrace ();
+		catch(Exception e) {
+			System.out.println(e);
 		}
 
 
 		return;
 	}
 
+
+	//Iteratively searches through a certain number of pages decided on my MAX_PAGES_TO_SEARCH variable
+	public void search(String url) {
+		while(this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
+			String currentUrl;
+			Spider leg = new Spider();
+			
+			if(this.pagesToVisit.isEmpty()) {
+				currentUrl = url;
+				this.pagesVisited.add(url);
+			}
+			else {
+				currentUrl = this.nextUrl();
+			}
+
+			System.out.println("\n\n");
+			System.out.println("PAGE VISITED SIZE: " + this.pagesVisited.size());
+			System.out.println("PAGE URL:          " + currentUrl);
+			System.out.println("\n\n");
+
+			try{
+				//this.printWords();
+				//this.printLinks();
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
+
+			leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in Spider
+			this.pagesToVisit.addAll(leg.getLinks());
+		}
+	}
+
+
+	private String nextUrl()
+	{
+		String nextUrl;
+		do {
+			nextUrl = this.pagesToVisit.remove(0);
+		} while(this.pagesVisited.contains(nextUrl));
+
+		this.pagesVisited.add(nextUrl);
+		return nextUrl;
+	}
+
+    public List<String> getLinks() {
+    	return this.linkResults;
+    }
+
+
 	private void extractMetaData() throws ParserException {
 		//Here we will gather data such as modified date, page size
 	}
 
+
+	//Extracts all the words in a given URL, may need to sift through words to get rid of useless characters i.e. brackets, periods, etc
 	private void extractKeywords(String url) throws ParserException {
 		//Here we will get all the keywords of the page and their freq
 		//and save them into the jdbm system
 		
 		//Extracts words/titles
-		//Vector<String> wordResults = new Vector<String>();
 		StringBean beanWord = new StringBean();
 		beanWord.setURL(url);
 		beanWord.setLinks(false);
@@ -91,42 +156,48 @@ public class Spider {
 		StringTokenizer st = new StringTokenizer(contents);
 		while (st.hasMoreTokens()) {
 			String tokenNext = st.nextToken();
-			wordResults.add(tokenNext);
+			this.wordResults.add(tokenNext);
+
+			//THIS IS WHERE YOU SEND DATA TO DATABASE!!
 			//System.out.println(tokenNext);
 		}
 
 		return;
 	}
+
 	
+	//Extracts all the links in a given URL
 	private void extractLinks(String url) throws ParserException {
 		//Here we shall extract the links to other pages and record
 		//them into the database :)
 		
 		//Extracts links
-		//Vector<String> linkResults = new Vector<String>();
 		LinkBean beanLinks = new LinkBean();
 		beanLinks.setURL(url);
 		URL[] urls = beanLinks.getLinks();
 		for (URL s : urls) {
-			linkResults.add(s.toString());
-			//System.out.println(s.toString());
+			this.linkResults.add(s.toString());
+
+			//prints out links in the page
+			System.out.println(s.toString());
 		}
 
 		return;
 	}
 
-
+	//NEED TO FIX, MAYBE?
 	public void printWords() throws ParserException{
-		for (Object s : wordResults) {
+		for (Object s : this.wordResults) {
 			System.out.println(s);
 		}
 
 		return;
 	}
 
+
 	//Prints all links in a given page, primarily used for testing
 	public void printLinks() throws ParserException{
-		for (Object s : linkResults) {
+		for (Object s : pagesToVisit) {
 			System.out.println(s);
 		}
 
